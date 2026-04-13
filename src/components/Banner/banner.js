16721dd99a0f7926ea3f4e4fcc02e1ce6e731dce
Alex <sheph@ukr.net>
2026-04-13 17:09:49 +0200

    added description to the banner

--- src/components/Banner/banner.js
+++ src/components/Banner/banner.js
@@ -1,5 +1,6 @@
 import './banner.scss';
 import mainBannerImage from '../../assets/banner/banner_main-page.webp';
+import { getCheckoutUrl, getProductUrl } from '../../router/routes';
 
 export default class Banner {
 	render() {
@@ -10,6 +11,9 @@ export default class Banner {
 	}
 
 	template() {
+		const featuredProductUrl = getProductUrl(1);
+		const checkoutUrl = getCheckoutUrl();
+
 		return /*html*/ `
 			<div class="banner__inner">
 				<img
@@ -19,6 +23,17 @@ export default class Banner {
 					loading="eager"
 					decoding="async"
 				/>
+				<div class="banner__overlay"></div>
+				<div class="banner__content cont">
+					<p class="banner__eyebrow">New season</p>
+					<h1 class="banner__title">Run the city in your next Nike pair</h1>
+					<p class="banner__lead">
+						Discover street-ready silhouettes, performance cushioning, and fresh drops selected for this month.
+					</p>
+					<div class="banner__actions">
+						<a href="${featuredProductUrl}" class="banner__btn banner__btn--solid">Shop Featured</a>
+					</div>
+				</div>
 			</div>
 		`;
 	}
